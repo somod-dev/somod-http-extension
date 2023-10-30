7014ce2ff20f69d8ddbe26e886a3345f4006055f
Raghavendra K R <raghavendra@sodaru.com>
2023-10-30 15:13:10 +0530

    added schemas/http-routes.json and made the middleware access to public

--- lib/routes-schema.ts
+++ lib/routes-schema.ts
@@ -1,4 +1,5 @@
 import { JSONSchema7 } from "json-schema";
+import routesSchema from "../schemas/http-routes.json";
 
 export type Routes = Record<
   string,
@@ -21,53 +22,4 @@ export type Routes = Record<
   >
 >;
 
-export const schema: JSONSchema7 = {
-  $schema: "http://json-schema.org/draft-07/schema",
-  $id: "https://somod-http-extension.sodev.com/routes-schema.json",
-  title: "JSON Schema for rotues configuration",
-  type: "object",
-  additionalProperties: false,
-  patternProperties: {
-    "^.*$": {
-      type: "object",
-      minProperties: 1,
-      additionalProperties: false,
-      patternProperties: {
-        "^(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)$": {
-          type: "object",
-          properties: {
-            parameters: {
-              type: "array",
-              minItems: 1,
-              items: {
-                type: "object",
-                required: ["in", "name", "schema", "required"],
-                properties: {
-                  in: { enum: ["path", "query", "header"] },
-                  name: {
-                    type: "string",
-                    pattern: "^[a-zA-Z0-9_-]+$"
-                  },
-                  schema: {
-                    $ref: "http://json-schema.org/draft-07/schema"
-                  },
-                  required: {
-                    type: "boolean"
-                  }
-                }
-              }
-            },
-            body: {
-              type: "object",
-              required: ["schema"],
-              properties: {
-                parser: { enum: ["text", "json", "formdata"] },
-                schema: { $ref: "http://json-schema.org/draft-07/schema" }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-};
+export const schema = routesSchema as JSONSchema7;
